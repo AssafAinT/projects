9724e343d48bc0b2f29377ae95d9695bb239a07c
assafainhoren <Assafain@gmail.com>
2022-12-11 07:39:58 +0200

    pig_game_rolled_reset_game

--- pig_game/script.js
+++ pig_game/script.js
@@ -1,9 +1,10 @@
 'use strict';
 function resetPage(){
-
+    // playing = true;
     scoreOneEl.textContent = 0;
     scoreTwoEl.textContent = 0;
     diceRoll.classList.add('hidden');
+    playerZero.classList.add('player--active');
 }
 const switchPlayer = function(){
     document.getElementById('current--' + activePlayer).textContent = 0;
@@ -13,6 +14,12 @@ const switchPlayer = function(){
     //toggling will ensure that the correct user is lighted
     playerZero.classList.toggle('player--active');
     playerOne.classList.toggle('player--active');
+}
+
+const showWhoOwn = function(){
+    document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+    document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+    document.getElementById('name--' + activePlayer).textContent += 'WINNED!'; 
 }
 const playerZero = document.querySelector('.player--0');
 const playerOne = document.querySelector('.player--1');
@@ -29,39 +36,48 @@ const curr1El = document.getElementById('current--1');
 const scores = [0, 0];
 let currScore = 0;
 let activePlayer = 0;
+let playing = true;
 //two different methods to gain elemnt by id- one is more faster
 
 //set initial values 
 resetPage();
-
 btnRoll.addEventListener('click', function(){
-    //1.generate new dice on the screen 
-    const diceRand = Math.trunc(Math.random() * 6) + 1;
-    diceRoll.classList.remove('hidden');
-    //manipulating html attribute in js 
-    //change to template literal
-    diceRoll.src = 'dice-' + diceRand+'.png';
-    //2. check if had the value of 1 , if true switch player
-    if (diceRand !== 1){
-        currScore += diceRand;
-        //choosing player dynamically
-        document.getElementById('current--' + activePlayer).textContent = currScore;
-        
-    } else {
-        
-        switchPlayer();
+    if (playing) {
+        //1.generate new dice on the screen 
+        const diceRand = Math.trunc(Math.random() * 6) + 1;
+        diceRoll.classList.remove('hidden');
+        //manipulating html attribute in js 
+        //change to template literal
+        diceRoll.src = 'dice-' + diceRand+'.png';
+        //2. check if had the value of 1 , if true switch player
+        if (diceRand !== 1){
+            currScore += diceRand;
+            //choosing player dynamically
+            document.getElementById('current--' + activePlayer).textContent = currScore;
+            
+        } else {
+            
+            switchPlayer();
+        }
     }
 });
 
 btnHold.addEventListener('click', function(){
-    scores[activePlayer] += currScore; 
-    document.getElementById('score--'+ activePlayer).textContent =scores[activePlayer];
+    if (playing) {
 
-    if (scores[activePlayer] < 100){
-        switchPlayer();
-
-    }else if (scores[activePlayer] >= 100) {
-        document.getElementById('name--' + activePlayer).textContent += 'WINNED!'; 
+        scores[activePlayer] += currScore; 
+        document.getElementById('score--'+ activePlayer).textContent =scores[activePlayer];
+        
+        if (scores[activePlayer] < 20){
+            switchPlayer();
+            
+        }else if (scores[activePlayer] >= 20) {
+            playing = false;
+            showWhoOwn();
+        }   
     }
-    
+});
+
+btnNew.addEventListener('click', function(){
+    resetPage();
 });
